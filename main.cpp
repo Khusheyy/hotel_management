da039808dd0a5ab95b8248013fea4531b2a745f8
Khushi Purohit <164623119+Khusheyy@users.noreply.github.com>
2025-04-18 21:00:12 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
-// i am this , this is me
+#include <iostream>
+using namespace std;
+
+int main() {
+    int limit;
+
+    cout << "Enter the limit: ";
+    cin >> limit;
+
+    cout << "Odd numbers up to " << limit << " are:" << endl;
+    for (int i = 1; i <= limit; i += 2) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
